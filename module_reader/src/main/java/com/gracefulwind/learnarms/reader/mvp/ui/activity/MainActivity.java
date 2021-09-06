e7bc8ecba200d7559ec14c7f70b89ca38485500f
Gracefulwind <429344332@qq.com>
2021-09-06 17:48:48 +0800

    [feat] 添加自定义图案
    利用span添加图案

--- module_reader/src/main/java/com/gracefulwind/learnarms/reader/mvp/ui/activity/MainActivity.java
+++ module_reader/src/main/java/com/gracefulwind/learnarms/reader/mvp/ui/activity/MainActivity.java
@@ -1,13 +1,33 @@
 package com.gracefulwind.learnarms.reader.mvp.ui.activity;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Html;
+import android.text.Selection;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ImageSpan;
+import android.view.InflateException;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.gracefulwind.learnarms.commonsdk.core.RouterHub;
+import com.gracefulwind.learnarms.commonsdk.utils.LogUtil;
+import com.gracefulwind.learnarms.commonsdk.utils.StringUtil;
+import com.gracefulwind.learnarms.commonsdk.utils.Utils;
+import com.gracefulwind.learnarms.reader.R2;
 import com.gracefulwind.learnarms.reader.di.component.DaggerMainComponent;
+import com.gracefulwind.learnarms.reader.widget.SmartTextView;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 
@@ -16,8 +36,16 @@ import com.jess.arms.di.component.AppComponent;
 import com.gracefulwind.learnarms.reader.mvp.contract.MainContract;
 import com.gracefulwind.learnarms.reader.mvp.presenter.MainPresenter;
 import com.gracefulwind.learnarms.reader.R;
+import com.jess.arms.utils.ArmsUtils;
 
 import org.jetbrains.annotations.NotNull;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+import static com.jess.arms.utils.Preconditions.checkNotNull;
 
 /**
  * @ClassName: MainActivity
@@ -32,6 +60,17 @@ import org.jetbrains.annotations.NotNull;
  */
 @Route(path = RouterHub.Reader.HOME_ACTIVITY)
 public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.View {
+
+    @BindView(R2.id.ram_tv_clcik1)
+    TextView ramBtnClick1;
+    @BindView(R2.id.ram_tv_clcik2)
+    Button ramBtnClick2;
+
+    @BindView(R2.id.ram_et_test0)
+    EditText ramEtTest0;
+    @BindView(R2.id.ram_stv_test1)
+    SmartTextView ramStvTest1;
+
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
         DaggerMainComponent //如找不到该类,请编译一下项目
@@ -45,12 +84,25 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     @Override
     public int initView(@Nullable Bundle savedInstanceState) {
         return R.layout.reader_activity_main; //如果你不需要框架帮你设置 setContentView(id) 需要自行设置,请返回 0
+//        return 0; //如果你不需要框架帮你设置 setContentView(id) 需要自行设置,请返回 0
     }
+
+//     return R.layout.reader_activity_main;
+
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
-        //setToolBar(toolbar, "Main");
+//        try{
+//            this.setContentView(R.layout.reader_activity_main);
+//            Unbinder mUnbinder = ButterKnife.bind(this);
+//        }catch (Exception e){
+//            if (e instanceof InflateException) {
+//                throw e;
+//            }
+//            e.printStackTrace();
+//        }
 
+        //setToolBar(toolbar, "Main");
         initListener();
     }
 
@@ -59,7 +111,139 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     }
 
     @Override
-    public void showMessage(@NonNull @NotNull String s) {
-
+    public void showMessage(@NonNull @NotNull String message) {
+        checkNotNull(message);
+        ArmsUtils.snackbarText(message);
     }
+
+//==================================================================================================
+    @OnClick({R2.id.ram_tv_clcik1, R2.id.ram_tv_clcik2, R2.id.ram_tv_clcik3})
+    public void onViewClicked(View view) {
+        int id = view.getId();
+        if(R.id.ram_tv_clcik1 == id){
+            String baseText = ramEtTest0.getText().toString();
+            String targetText = ramStvTest1.getText().toString();
+            if(StringUtil.isEmpty(baseText)){
+                targetText += "testetttttttttt=========================testeeeteeteee\r\n";
+            }else {
+                targetText += baseText + "";
+            }
+//            targetText = "testetttttttttt=========================testeeeteeteee\r\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\ntestetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n" +
+//                    "testetttttttttt=========================testeeeteeteee\n";
+//            ramEtTest0.setText(targetText);
+            ramStvTest1.setText(targetText);
+        }else if (R.id.ram_tv_clcik2 == id){
+            //
+            System.out.println("=========================");
+            String stvStr = ramStvTest1.getText().toString();
+            Editable editText = ramStvTest1.getText();
+            int selectionStart = Selection.getSelectionStart(editText);
+            int selectionEnd = Selection.getSelectionEnd(editText);
+            editText.append("\r\n");
+            ramStvTest1.setText(editText);
+            System.out.println("=========================");
+        }else if (R.id.ram_tv_clcik3 == id){
+            Editable editText = ramStvTest1.getText();
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                editText.append(Html.fromHtml("<img src=\"/test_span\" width=\"304\" height=\"228\">"
+//                        , Html.FROM_HTML_MODE_LEGACY, new Html.ImageGetter() {
+//                            @Override
+//                            public Drawable getDrawable(String source) {
+//                                Drawable drawable = getResources().getDrawable(R.drawable.test_span);
+//                                drawable.setBounds(0,0,20, 20);
+//                                System.out.println("====");
+//                                return drawable;
+//                            }
+//                        }, null));
+//            }else {
+//                editText.append(Html.fromHtml("<img src=\"/test_span\" width=\"304\" height=\"228\">"));
+//            }
+//            addImageSpan(editText);
+            setImageWithSpan(editText);
+//            ramStvTest1.setText(Html.fromHtml(editText));
+//            ramStvTest1.setText(editText);
+//            ramStvTest1.setMovementMethod(LinkMovementMethod.getInstance());
+            ramStvTest1.setText(editText);
+        }
+    }
+
+    private void setImageWithSpan(Editable editText) {
+        SpannableStringBuilder ssb = new SpannableStringBuilder("\uFFFC");
+        Drawable drawable = getResources().getDrawable(R.drawable.test_span);
+        drawable.setBounds(0,0, 40, 40);
+        ImageSpan imageSpan = new ImageSpan(drawable, ImageSpan.ALIGN_BASELINE);
+        ssb.setSpan(imageSpan, 0, ssb.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        editText.append(ssb);
+    }
+
+
+    String h5Text = "<!DOCTYPE html>\n" +
+            "<html>\n" +
+            "<head> \n" +
+            "<meta charset=\"utf-8\"> \n" +
+            "<title>菜鸟教程(runoob.com)</title> \n" +
+            "</head>\n" +
+            "<body>\n" +
+            "\n" +
+            "<p>\n" +
+            "<a href=\"/index.html\">本文本</a> 是一个指向本网站中的一个页面的链接。</p>\n" +
+            "\n" +
+            "<p><a href=\"//www.microsoft.com/\">本文本</a> 是一个指向万维网上的页面的链接。</p>\n" +
+            "\n" +
+            "</body>\n" +
+            "</html>";
+
+    String h5TextImg = "<!DOCTYPE html>\n" +
+            "<html>\n" +
+            "<head>\n" +
+            "    <meta charset=\"utf-8\">\n" +
+            "    <title>菜鸟教程(runoob.com)</title>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "\n" +
+            "<h2>Norwegian Mountain Trip</h2>\n" +
+            "<img border=\"0\" src=\"/images/pulpit.jpg\" alt=\"Pulpit rock\" width=\"304\" height=\"228\">\n" +
+            "\n" +
+            "</body>\n" +
+            "</html>";
+
+
 }
---
+++ module_reader/src/main/java/com/gracefulwind/learnarms/reader/widget/SmartTextView.java
@@ -0,0 +1,276 @@
+package com.gracefulwind.learnarms.reader.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.text.Editable;
+import android.text.Html;
+import android.text.Layout;
+import android.text.Selection;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.StaticLayout;
+import android.text.TextDirectionHeuristics;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.method.ArrowKeyMovementMethod;
+import android.text.method.MovementMethod;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.gracefulwind.learnarms.commonsdk.utils.LogUtil;
+
+/**
+ * @ClassName: SmartTextView
+ * @Author: Gracefulwind
+ * @CreateDate: 2021/9/2
+ * @Description: ---------------------------
+ * 画上分割线，添加富文本图案。所有的手写字都是富文本的形式
+ * @UpdateUser:
+ * @UpdateDate: 2021/9/2
+ * @UpdateRemark:
+ * @Version: 1.0
+ * @Email: 429344332@qq.com
+ */
+public class SmartTextView extends android.support.v7.widget.AppCompatTextView {
+    public static final String TAG = SmartTextView.class.getName();
+
+    private final TextPaint paint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+    private int width;
+    private int height;
+
+    public SmartTextView(Context context) {
+        this(context, null);
+    }
+
+    public SmartTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 16842884);
+
+    }
+
+    public SmartTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setFocusableInTouchMode(true);
+    }
+
+    @Override
+    public boolean getFreezesText() {
+        return true;
+    }
+
+    @Override
+    protected boolean getDefaultEditable() {
+        return true;
+    }
+
+    @Override
+    protected MovementMethod getDefaultMovementMethod() {
+        return ArrowKeyMovementMethod.getInstance();
+    }
+
+    @Override
+    public Editable getText() {
+        CharSequence text = super.getText();
+        // This can only happen during construction.
+        if (text == null) {
+            return null;
+        }
+        if (text instanceof Editable) {
+            return (Editable) super.getText();
+        }
+        super.setText(text, BufferType.EDITABLE);
+        return (Editable) super.getText();
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+//        Spanned spanned = Html.fromHtml(text.toString());
+//        super.setText(spanned, BufferType.EDITABLE);
+        super.setText(text, BufferType.EDITABLE);
+    }
+
+    /**
+     * Convenience for {@link Selection#setSelection(Spannable, int, int)}.
+     */
+    public void setSelection(int start, int stop) {
+        Selection.setSelection(getText(), start, stop);
+    }
+
+    /**
+     * Convenience for {@link Selection#setSelection(Spannable, int)}.
+     */
+    public void setSelection(int index) {
+        Selection.setSelection(getText(), index);
+    }
+
+    /**
+     * Convenience for {@link Selection#selectAll}.
+     */
+    public void selectAll() {
+        Selection.selectAll(getText());
+    }
+
+    /**
+     * Convenience for {@link Selection#extendSelection}.
+     */
+    public void extendSelection(int index) {
+        Selection.extendSelection(getText(), index);
+    }
+
+    /**
+     * Causes words in the text that are longer than the view's width to be ellipsized instead of
+     * broken in the middle. {@link TextUtils.TruncateAt#MARQUEE
+     * TextUtils.TruncateAt#MARQUEE} is not supported.
+     *
+     * @param ellipsis Type of ellipsis to be applied.
+     * @throws IllegalArgumentException When the value of <code>ellipsis</code> parameter is
+     *      {@link TextUtils.TruncateAt#MARQUEE}.
+     * @see TextView#setEllipsize(TextUtils.TruncateAt)
+     *
+     * 文字过长的特效，避免跑马灯
+     */
+    @Override
+    public void setEllipsize(TextUtils.TruncateAt ellipsis) {
+        if (ellipsis == TextUtils.TruncateAt.MARQUEE) {
+            throw new IllegalArgumentException("EditText cannot use the ellipsize mode "
+                    + "TextUtils.TruncateAt.MARQUEE");
+        }
+        super.setEllipsize(ellipsis);
+    }
+
+    @Override
+    public void setOverScrollMode(int overScrollMode) {
+        super.setOverScrollMode(overScrollMode);
+    }
+
+    /**
+     * 继承自View的方法，只是把类名返回去而已
+     * */
+    @Override
+    public CharSequence getAccessibilityClassName() {
+//        //define in view
+//        super.getAccessibilityClassName();
+        return TAG;
+    }
+
+//    supportsAutoSizeText
+
+//    @Override
+//    protected  boolean setAutoSizeTextTypeWithDefaults(){
+//        super.setAutoSizeTextTypeWithDefaults();
+//        return false;
+//    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        LogUtil.d(TAG, "onSizeChanged, { old w,h = " + oldw + "," + oldh
+                    + " }        { new w,h = " +  w + "," + h);
+        this.width = w;
+        this.height = h;
+        int lineHeight = getLineHeight();
+        float fontHeight = getFontHeight(getTextSize());
+        LogUtil.d(TAG, "lineHeight = " + lineHeight + ",  fontHeight = " + fontHeight);
+//        if(h - oldh == lineHeight){
+//            ViewGroup.LayoutParams layoutParams = getLayoutParams();
+//            layoutParams.height += 2 * lineHeight;
+//        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //do my works
+        paint.setStyle(Paint.Style.FILL);
+        //todo:wd 这里是凑巧等于-1还是系统设置的-1?
+//        float textSize = getTextSize();
+//        float textHeight = getFontHeight(textSize) - 1;
+        float textHeight = getLineHeight();
+        int paddingTop = getPaddingTop();
+        int paddingBottom = getPaddingBottom();
+        int paddingLeft = getPaddingLeft();
+        int paddingRight = getPaddingRight();
+        LogUtil.d(TAG, "====onDraw====");
+////        int textViewLines = getTextViewLines(width);
+//        int height = getHeight();
+//        int measuredHeight = getMeasuredHeight();
+//        int lineHeight = getLineHeight();
+//        for(int x = 0; (paddingTop + paddingBottom + (x * textHeight)) < this.height; x++){
+            //如果我绘制的超出原来的大小，视图会变大
+        for(int x = 0; (paddingTop + paddingBottom + ((x-2) * textHeight)) < this.height; x++){
+//        for(int x = 0; x < textViewLines; x++){
+            canvas.drawLine(paddingLeft, paddingTop + x * textHeight, width - paddingRight, paddingTop + x * textHeight, paint);
+        }
+        
+        LogUtil.d(TAG, "==============");
+    }
+
+    /**
+     * 这里获取的是文字高度，和行高度有偏差
+     * */
+    public float getFontHeight(float fontSize)  {
+        Paint paint = new Paint();
+        paint.setTextSize(fontSize);
+        Paint.FontMetrics fm = paint.getFontMetrics();
+        return (float) Math.ceil(fm.descent - fm.ascent);
+    }
+
+
+    public int getTextViewLines(int textViewWidth) {
+        int width = textViewWidth - getCompoundPaddingLeft() - getCompoundPaddingRight();
+        StaticLayout staticLayout;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            staticLayout = getStaticLayout23(width);
+        } else {
+            staticLayout = getStaticLayout(width);
+        }
+        int lines = staticLayout.getLineCount();
+        int maxLines = getMaxLines();
+        if (maxLines > lines) {
+            return lines;
+        }
+        return maxLines;
+    }
+
+    /**
+     * sdk>=23
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private StaticLayout getStaticLayout23(int width) {
+        StaticLayout.Builder builder = StaticLayout.Builder.obtain(getText(),
+                0, getText().length(), getPaint(), width)
+                .setAlignment(Layout.Alignment.ALIGN_NORMAL)
+                .setTextDirection(TextDirectionHeuristics.FIRSTSTRONG_LTR)
+                .setLineSpacing(getLineSpacingExtra(), getLineSpacingMultiplier())
+                .setIncludePad(getIncludeFontPadding())
+                .setBreakStrategy(getBreakStrategy())
+                .setHyphenationFrequency(getHyphenationFrequency())
+                .setMaxLines(getMaxLines() == -1 ? Integer.MAX_VALUE : getMaxLines());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setJustificationMode(getJustificationMode());
+        }
+        if (getEllipsize() != null && getKeyListener() == null) {
+            builder.setEllipsize(getEllipsize())
+                    .setEllipsizedWidth(width);
+        }
+        return builder.build();
+    }
+
+    /**
+     * sdk<23
+     */
+    private StaticLayout getStaticLayout(int width) {
+        return new StaticLayout(getText(),
+                0, getText().length(),
+                getPaint(), width, Layout.Alignment.ALIGN_NORMAL,
+                getLineSpacingMultiplier(),
+                getLineSpacingExtra(), getIncludeFontPadding(), getEllipsize(),
+                width);
+    }
+}
